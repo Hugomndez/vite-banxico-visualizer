9d91dfcebd66938ef7231429267b1d39242db6d7
Hugomndez <hello@hugomendez.dev>
2022-07-05 23:24:56 -0500

    Fix css loading after scripts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
+import './styles/globals.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { FetchProvider } from './contexts';
-
-import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
